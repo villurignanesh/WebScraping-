d1d07ba5e630f3b64da93e4d9f469a8d8f74e966
Villuri Gnanesh <46528428+villurignanesh@users.noreply.github.com>
2020-06-01 21:06:56 +0530

    Update urllinks.py

--- urllinks.py
+++ urllinks.py
@@ -1,21 +1,24 @@
-# To run this, download the BeautifulSoup zip file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
-# Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
+position = int(input("Enter position:"))-1
+count = int(input("Enter count:"))
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
-
-# Retrieve all of the anchor tags
+Sequence = []
 tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+for i in range(count):
+    link = tags[position].get('href', None)
+    print("Retrieving:",link)
+    Sequence.append(tags[position].contents[0])
+    html = urllib.request.urlopen(link, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    link = tags[position].get('href', None)
+print(Sequence[-1])
